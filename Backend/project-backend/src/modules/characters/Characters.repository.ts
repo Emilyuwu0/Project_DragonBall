178af8076005d4497aa238e7ba062a3b6e330501
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2024-10-08 15:36:30 -0500

    transformaciones_añadidas

--- Backend/project-backend/src/modules/characters/Characters.repository.ts
+++ Backend/project-backend/src/modules/characters/Characters.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CharacterEntity } from 'src/entities/Characters.entity';
 import { PlanetEntity } from 'src/entities/Planets.entity';
@@ -57,22 +57,94 @@ export class CharactersRepository {
   async getCharacters(page = 1, limit = 5) {
     const offSet = (page - 1) * limit;
 
-    const [characters, total] = await this.characterDBRepository.findAndCount({
-      skip: offSet,
-      take: limit,
+    const [FoundCharacters, total] =
+      await this.characterDBRepository.findAndCount({
+        skip: offSet,
+        take: limit,
+      });
+
+    const transformaciones = await this.transformationRepository.find({
+      relations: ['character'],
     });
+
+    if (!transformaciones)
+      throw new NotFoundException('no hay transformaciones');
+
+    const characters = FoundCharacters.map((element) => {
+      const characterTransformations = transformaciones.filter(
+        (transformation) => {
+          return transformation.character?.id === element.id; //
+        },
+      );
+      return {
+        id: element.id,
+        name: element.name,
+        image: element.image,
+        ki: element.ki,
+        maxKi: element.maxKi,
+        race: element.race,
+        gender: element.gender,
+        description: element.description,
+        affiliation: element.affiliation,
+        transformations: characterTransformations.map((transformation) => ({
+          id: transformation.id,
+          name: transformation.name,
+          ki: transformation.ki,
+          image: transformation.image,
+        })),
+      };
+    });
+
     return {
-      items: characters, // Los personajes paginados
+      items: characters,
       meta: {
-        totalItems: total, // Número total de personajes
-        totalPages: Math.ceil(total / limit), // Total de páginas
-        currentPage: page, // Página actual
-        limit: limit, // Límite de personajes por página
+        totalItems: total,
+        totalPages: Math.ceil(total / limit),
+        currentPage: page,
+        limit: limit,
       },
     };
   }
 
   async allCharacters() {
-    return await this.characterDBRepository.find();
+    const FoundCharacters = await this.characterDBRepository.find();
+
+    if (!FoundCharacters)
+      throw new NotFoundException('Ejecuta el Get Characters/seeder');
+
+    const transformations = await this.transformationRepository.find({
+      relations: ['character'],
+    });
+
+    if (!transformations)
+      throw new NotFoundException('no hay transformaciones');
+
+    const characters = FoundCharacters.map((element) => {
+      const characterTransformations = transformations.filter(
+        (transformation) => {
+          return transformation.character?.id === element.id; //
+        },
+      );
+      console.log(characterTransformations);
+      return {
+        id: element.id,
+        name: element.name,
+        image: element.image,
+        ki: element.ki,
+        maxKi: element.maxKi,
+        race: element.race,
+        gender: element.gender,
+        description: element.description,
+        affiliation: element.affiliation,
+        transformations: characterTransformations.map((transformation) => ({
+          id: transformation.id,
+          name: transformation.name,
+          ki: transformation.ki,
+          image: transformation.image,
+        })),
+      };
+    });
+
+    return characters;
   }
 }
